87b85827738438903480bb54383941c12c775d11
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-09-26 17:11:03 -0500

    display posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
 
   def display_content
     @section = params[:section]
+    @posts = Post.order('created_at DESC').all
     @message = Message.new
 
     respond_to do |format|
