163b985c510944953b2077a38a9d6afca9a75080
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-09-28 02:47:34 -0500

    allow destruction of nested attributes

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,5 +2,5 @@ class Page < ActiveRecord::Base
   has_many :texts
   has_many :snippets
 
-  accepts_nested_attributes_for :texts, :snippets
+  accepts_nested_attributes_for :texts, :snippets, allow_destroy: true
 end
