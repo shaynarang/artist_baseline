375c1dae1d5a1f8e415da8c68fac26ba2d5894bd
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-09-28 00:13:54 -0500

    create pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_050352) do
+ActiveRecord::Schema.define(version: 2018_09_28_051218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2018_09_28_050352) do
     t.datetime "updated_at", null: false
     t.index ["imageable_id", "imageable_type"], name: "index_images_on_imageable_id_and_imageable_type"
     t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.integer "priority"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
