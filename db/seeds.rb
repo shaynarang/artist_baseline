377c5da6b6f5b487fcf490bc94497a74bb4b9cfa
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-09-29 16:43:21 -0500

    seed pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development? && !AdminUser.exists?(email: 'admin@example.com')
+
+Page.create!(title: 'News') unless Page.exists?(title: 'News')
+
+Page.create!(title: 'Contact') unless Page.exists?(title: 'Contact')
