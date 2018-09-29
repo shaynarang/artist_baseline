031f0ca7bc2ed1b152a7c9fc775153a03d5fc764
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-09-29 13:39:20 -0500

    add section model and relationships

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,5 @@
 class Page < ActiveRecord::Base
-  has_many :texts
-  has_many :snippets
+  has_many :sections
 
-  accepts_nested_attributes_for :texts, :snippets, allow_destroy: true
+  accepts_nested_attributes_for :sections, allow_destroy: true
 end
