786696ae485a90dd9ccfcd914ec8daa1d3122155
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-09-29 14:20:18 -0500

    refactor activeadmin page views

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,6 +1,26 @@
 ActiveAdmin.register Page do
   permit_params :title, :priority, :sections_attributes => [:id, :kind, :content, :priority, :_destroy]
 
+  index do
+    column :title
+    column :priority
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :priority
+      if page.sections.any?
+        table_for page.sections do
+          column :kind
+          column :content
+          column :priority
+        end
+      end
+    end
+  end
+
   form do |f|
     f.inputs "Page Details" do
       li f.semantic_errors *f.object.errors.keys
