e9a30a2c0d766eae05131e06f91bc8bfdbc30496
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-09-29 13:35:36 -0500

    rename section to page name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def display_content
-    @section = params[:section]
+    @page_name = params[:page_name]
     @page = Page.find_by(id: params[:page_id])
     @posts = Post.order('created_at DESC').all
     @message = Message.new
