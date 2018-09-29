6a1a0ffd093f2a91fe9d6a95061b3037b77e2d13
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-09-29 15:07:39 -0500

    include and configure acts_as_list

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,6 @@
 class Page < ActiveRecord::Base
+  acts_as_list
+
   has_many :sections
 
   accepts_nested_attributes_for :sections, allow_destroy: true
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,7 @@
 class Section < ActiveRecord::Base
   belongs_to :page
 
+  acts_as_list scope: :page
+
   KINDS = %w(Snippet Text)
 end
