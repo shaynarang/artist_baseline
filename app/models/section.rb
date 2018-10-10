be21cbc64611480e576423792b1db4029553de54
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2018-10-09 20:02:57 -0500

    declare relationship, accept nested attributes, update kinds collection

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,7 +1,10 @@
 class Section < ActiveRecord::Base
   belongs_to :page
+  has_many :photos, :as => :imageable, :dependent => :destroy
 
   acts_as_list scope: :page
 
-  KINDS = %w(Snippet Text)
+  accepts_nested_attributes_for :photos, :allow_destroy => true
+
+  KINDS = %w(Snippet Text Photo)
 end
