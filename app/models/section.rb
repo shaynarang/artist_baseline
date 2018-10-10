cf863786fa42518881aeb21ec3584b8968f9a3b0
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-10-10 00:31:29 -0500

    remove extraneous content, photos, and verify presence

--- app/models/section.rb
+++ app/models/section.rb
@@ -6,5 +6,27 @@ class Section < ActiveRecord::Base
 
   accepts_nested_attributes_for :photos, :allow_destroy => true
 
-  KINDS = %w(Snippet Text Photo)
+  before_update :remove_content, :remove_photos
+
+  validate :presence_of_content_or_photos
+
+  KINDS = %w(Photo Snippet Text)
+
+  private
+
+  def remove_content
+    return unless kind == 'Photo'
+    self.content = nil
+  end
+
+  def remove_photos
+    return unless kind == 'Snippet' || kind == 'Text'
+    self.photos.map(&:remove_image!)
+    self.photos.map(&:save)
+  end
+
+  def presence_of_content_or_photos
+    return if self.content.present? || self.photos.any?
+    errors.add(:base, 'Please add some text, a snippet, or a photo')
+  end
 end
