ea09e6ba9c002ae6e4143a2369e864e61d0de4a4
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-10-10 00:57:39 -0500

    add published to pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_224652) do
+ActiveRecord::Schema.define(version: 2018_10_10_053616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2018_10_09_224652) do
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "published", default: false
   end
 
   create_table "photos", force: :cascade do |t|
