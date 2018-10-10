8c7678985514a2366da3b65f2096901a8ceb105f
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2018-10-09 20:01:55 -0500

    update form for inclusion of sections and photos

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Page do
 
   config.filters = false
 
-  permit_params :title, :position, :sections_attributes => [:id, :kind, :content, :position, :_destroy]
+  permit_params :title, :position, :sections_attributes => [:id, :kind, :content, :position, :_destroy, :photos_attributes => [:id, :imageable_id, :imageable_type, :image, :remote_image_url, :_destroy]]
 
   index as: :reorderable_table do
     column :title
@@ -31,10 +31,17 @@ ActiveAdmin.register Page do
       li f.semantic_errors *f.object.errors.keys
       f.input :title
     end
-    f.has_many :sections, sortable: :position, sortable_start: 1 do |section|
-      section.input :kind, :as => :select, :collection => Section::KINDS
-      section.input :content
-      section.input :_destroy, :as => :boolean, :required => false, :label => 'Remove'
+    f.has_many :sections, :multipart => true, sortable: :position, sortable_start: 1 do |s|
+      s.input :kind, :as => :select, :collection => Section::KINDS, :include_blank => false, :input_html => { class: 'section_kind_select'}
+      s.input :content, :input_html => { class: 'content_text_area'}
+
+      s.has_many :photos, new_record: false do |p|
+        p.input :image, :label => 'Upload Image'
+        p.input :remote_image_url, :label => 'Paste Image URL', :hint => (image_tag(p.object.image.url(:thumb)) if p.object.image && !p.object.image.url.blank?)
+        p.input :_destroy, as: :boolean, required: :false, label: 'Remove image'
+      end
+
+      s.input :_destroy, :as => :boolean, :required => false, :label => 'Remove'
     end
     f.actions do
       f.cancel_link(:back)
