dcdf3d8d32ea7a281b70d5d7c940ba14e39ca300
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-10-24 23:30:36 -0500

    specify relationship

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,7 +1,9 @@
 class Site < ActiveRecord::Base
   has_many :photos, :as => :imageable, :dependent => :destroy
+  has_many :themes
 
   accepts_nested_attributes_for :photos, :allow_destroy => true
+  accepts_nested_attributes_for :themes, :allow_destroy => true
 
   validates_presence_of :title
 end
