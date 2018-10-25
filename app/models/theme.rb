d75f996c7cfe7a739b8d0c9f3fc739f2ed46c109
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-10-25 01:01:48 -0500

    validate presence of name, add published scope, unpublish theme before save of newly published theme

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,3 +1,18 @@
 class Theme < ActiveRecord::Base
   belongs_to :site
+
+  validates_presence_of :name
+  before_save :unpublish_theme
+
+  scope :published, -> { where(published: true) }
+
+  private
+
+  def unpublish_theme
+    # if a theme has newly been marked as published
+    # we need to unmark any old published theme
+    return unless self.published
+    published_themes = self.site.themes.published
+    published_themes[0].toggle!(:published) if published_themes.any?
+  end
 end
