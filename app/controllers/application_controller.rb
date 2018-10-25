3db8f0479beb451f40d47b881096dafcfb3a1422
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2018-10-25 02:03:38 -0500

    add controller and view to render theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_theme
+
+  def set_theme
+    @theme = Site.first&.themes&.published[0]
+  end
 end
