2f81ec843dc8c2919b7d06faf20758f055ccf633
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2020-09-10 15:27:23 -0500

    update region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ else
       provider: 'AWS', 
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], 
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'us-west-2'
+      region: 'us-east-2'
     } 
     config.fog_directory = ENV['AWS_S3_BUCKET']
     config.fog_public = false 
