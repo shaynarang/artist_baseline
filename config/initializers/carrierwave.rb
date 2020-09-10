0d16b17c03825d1da0ebb6139cbba05db2d7cf3c
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2020-09-10 15:31:31 -0500

    reorder config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,13 @@ if Rails.env.test? or Rails.env.development?
   end
 else
   CarrierWave.configure do |config| 
-    config.storage = :fog
     config.fog_credentials = { 
       provider: 'AWS', 
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], 
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'us-east-2'
     } 
+    config.storage = :fog
     config.fog_directory = ENV['AWS_S3_BUCKET']
     config.fog_public = false 
   end 
