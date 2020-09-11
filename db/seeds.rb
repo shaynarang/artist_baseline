5cc401ecf34eb2533dbdcafbc64d2df130cd6965
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2020-09-10 23:21:27 -0500

    replace first or create with find or create by

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env.development?
-  AdminUser.first_or_create(
+  AdminUser.find_or_create_by(
     email: 'admin@example.com',
     password: 'password',
     password_confirmation: 'password'
   )
 end
 
-Site.first_or_create(title: 'Your Site')
+Site.find_or_create_by(title: 'Your Site')
 
-Page.first_or_create(title: 'News')
+Page.find_or_create_by(title: 'News')
 
-Page.first_or_create(title: 'Contact')
+Page.find_or_create_by(title: 'Contact')
 
-Theme.first_or_create(
+Theme.find_or_create_by(
   site: Site.last,
   font_url: "https://fonts.googleapis.com/css?family=Arvo|Raleway|Roboto+Mono|Russo+One|Unlock",
   nav_background_color: "black",
   nav_text_color: "mintcream",
   main_background_color: "mintcream",
   content_text_color: "black",
-  border_color: "black", published: true,
+  border_color: "black",
+  published: true,
   font_family: "Roboto Mono",
   title_font_url: "https://fonts.googleapis.com/css?family=Arvo|Raleway|Roboto+Mono|Russo+One|Unlock",
   title_font_family: "Roboto Mono"
