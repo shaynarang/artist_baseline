542a133a6c320a8bbbedcff8c0e5e116fb11be01
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2020-09-10 23:11:02 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development? && !AdminUser.exists?(email: 'admin@example.com')
+if Rails.env.development?
+  AdminUser.first_or_create(
+    email: 'admin@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
 
-Site.create!(title: 'Your Site')
+Site.first_or_create(title: 'Your Site')
 
-Page.create!(title: 'News') unless Page.exists?(title: 'News')
+Page.first_or_create(title: 'News')
 
-Page.create!(title: 'Contact') unless Page.exists?(title: 'Contact')
+Page.first_or_create(title: 'Contact')
+
+Theme.first_or_create(
+  site: Site.last.id,
+  font_url: "https://fonts.googleapis.com/css?family=Arvo|Raleway|Roboto+Mono|Russo+One|Unlock",
+  nav_background_color: "black",
+  nav_text_color: "mintcream",
+  main_background_color: "mintcream",
+  content_text_color: "black",
+  border_color: "black", published: true,
+  font_family: "Roboto Mono",
+  title_font_url: "https://fonts.googleapis.com/css?family=Arvo|Raleway|Roboto+Mono|Russo+One|Unlock",
+  title_font_family: "Roboto Mono"
+)
